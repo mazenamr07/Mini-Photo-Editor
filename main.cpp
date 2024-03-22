3c576ad846df9a606a7913f36c64c6699ea30783
Mazen Amr <160724290+mazenamr07@users.noreply.github.com>
2024-03-23 01:42:57 +0200

    Updated stb
    Updated stb files to work with ease and two folders for img

--- main.cpp
+++ main.cpp
@@ -1,9 +1,57 @@
-#include <iostream>
+#include "Image_Class.h"
+#include <vector>
 
-#include "stb_image.h"
-#include "stb_image_write.h"
+using namespace std;
+
+void menu() {
+    vector<unsigned char> pixels;
+    Image img("img/frieren.jpg");
+    Image square(250, 250);
+    Image imgBro(720, 1080);
+    Image imgRT90(img.height, img.width);
+    Image imgRT270(img.height, img.width);
+
+    for (int i = 0; i < img.width; i++) {
+        for (int j = 0; j < img.height; j++) {
+            for (int k = 0; k < 3; k++) {
+                imgBro(720 - i - 1, 1080 - j - 1, k) = img(i, j, k);
+                imgRT90(j, 720 - i - 1 ,k) = img(i, j, k);
+                imgRT270(1080 - j - 1, i ,k) = img(i, j, k);
+            }
+        }
+    }
+
+    square(125, 35, 0) = img(0, 0, 0);
+    square(125, 35, 1) = img(0, 0, 1);
+    square(125, 35, 2) = img(0, 0, 2);
+
+
+    square.saveImage("saved img/cSquare.png");
+    imgBro.saveImage("saved img/Mafggbombo.png");
+    imgRT90.saveImage("saved img/RTfrieren.jpg");
+    imgRT270.saveImage("saved img/RT270frieren.jpg");
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Image img(250,250);
+
+    for (int i = 0; i < img.width; i++) {
+        for (int j = 0; j < img.height; j++) {
+
+            for (int k = 0; k < 3; k++) {
+                img(i, j, k) = 255;
+            }
+        }
+    }
+    for (int i = 0; i < img.width; i++) {
+        for (int j = 0; j < img.height; j++) {
+            img(i, j, 0) = 200;
+            img(i, j, 1) = 63;
+            img(i, j, 2) = 154;
+        }
+    }
+
+
+    img.saveImage("saved img/cSquare.png");
     return 0;
 }
--- stb_image_write.h
+++ stb_image_write.h
@@ -1,3 +1,12 @@
+
+/**
+ * this is an edited version of the stb_image_write for FCAI
+ *
+ */
+
+
+
+
 /* stb_image_write - v1.16 - public domain - http://nothings.org/stb
    writes out PNG/BMP/TGA/JPEG/HDR images to C stdio - Sean Barrett 2010-2015
                                      no warranty implied; use at your own risk
