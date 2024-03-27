e0db1c4b578554ec33cebccd48b4f114d7f7f06b
Mazen Amr <160724290+mazenamr07@users.noreply.github.com>
2024-03-27 16:11:12 +0200

    Adjusted functions code to be at the end of the file

--- main.cpp
+++ main.cpp
@@ -1,151 +1,50 @@
+//
+
 #include <sstream>
 #include <vector>
 #include "Image_Class.h"
 
 using namespace std;
 
-bool choiceCheck(const string& choice) {
-    vector<string> choices = {"0", "1", "2", "3", "4", "5"};
+// Function to check choice validity
+bool choiceCheck(const string& choice);
 
-    for (const string& i : choices) {
-        if (choice == i) {
-            return true;
-        }
-    }
-    return false;
-}
+// Function to check image extension validity
+bool exCheck(const string& ex);
 
-bool exCheck(const string& ex) {
-    if (ex == "png") {
-        return true;
-    } else if (ex == "jpg") {
-        return true;
-    } else if (ex == "jpeg") {
-        return true;
-    } else if (ex == "bmp") {
-        return true;
-    }
-    return false;
-}
+// Function to change image extension
+void exChange(Image img, int counter, const string& ex, string& filename, Image& jpgImg);
 
-void exChange(Image img, int counter, const string& ex, string& filename, Image& jpgImg) {
-    filename = "temp/tempPic";
-    filename += to_string(counter);
-    filename += ex;
-    img.saveImage(filename);
+// Function to apply Gray scale filter
+void GrayScale(Image img, Image& grayImg);
 
-    jpgImg.loadNewImage(filename);
-}
+// Function to apply Black & White filter
 
-void GrayScale(Image img, Image& grayImg) {
-    for (int i = 0; i < img.width; i++) {
-        for (int j = 0; j < img.height; j++) {
+// Function to merge two images
+void Merge(Image img1, Image img2, float opacity, Image& mergedImg);
 
-            unsigned int grayAvg = 0;
-            for (int k = 0; k < 3; k++) {
-                grayAvg += img(i, j, k);
-            }
+// Function to adjust an image brightness
+void Brightness(Image img, float bright, Image& brightImg);
 
-            grayAvg /= 3;
-            for (int k = 0; k < 3; k++) {
-                grayImg(i, j, k) = grayAvg;
-            }
-        }
-    }
-}
+// Function to apply Edge Detect filter
+void EdgeDetect(Image grayedImg, Image& edgeImg);
 
-void Merge(Image img1, Image img2, float opacity, Image& mergedImg) {
-    for (int i = 0; i < img1.width; i++) {
-        for (int j = 0; j < img1.height; j++) {
+// Function to apply Inverted Colors filter
+void InvertColor(Image myimage, Image& img);
 
-            double color;
-            for (int k = 0; k < 3; k++) {
-                color = (1 - opacity) * img1(i, j, k) + opacity * img2(i, j, k);
-                mergedImg(i, j, k) = color;
-            }
-        }
-    }
-}
+// Functions to Rotate an image
+void imageRotate90(Image img, Image& rtImg);
+void imageRotate180(Image img, Image& rtImg);
+void imageRotate270(Image img, Image& rtImg);
 
-void Brightness(Image img, float bright, Image& brightImg) {
-    for (int i = 0; i < img.width; i++) {
-        for (int j = 0; j < img.height; j++) {
-            for (int k = 0; k < 3; k++) {
-                int bb = img(i, j, k) * bright;
-                if (bb > 255) {
-                    bb = 255;
-                }
-                brightImg(i, j, k) = bb;
-            }
-        }
-    }
-}
-
-void EdgeDetect(Image grayedImg, Image& edgeImg) {
-    for (int i = 0; i < grayedImg.width; i++) {
-        for (int j = 0; j < grayedImg.height; j++) {
-            if (grayedImg(i + 1, j, 0) - grayedImg(i, j, 0) > 10
-            or grayedImg(i, j + 1, 0) - grayedImg(i, j, 0) > 10) {
-                edgeImg(i, j, 0) = 0;
-                edgeImg(i, j, 1) = 0;
-                edgeImg(i, j, 2) = 0;
-            }
-            else {
-                edgeImg(i, j, 0) = 255;
-                edgeImg(i, j, 1) = 255;
-                edgeImg(i, j, 2) = 255;
-            }
-        }
-    }
-}
-
-void InvertColor(Image myimage, Image& img){
-    for (int i=0;i<myimage.width;i++)
-    {
-        for (int j=0;j<myimage.height;j++)
-        {
-            for (int k=0;k<myimage.channels;k++)
-            {
-                img(i,j,k)=255-myimage(i,j,k);
-            }
-        }
-    }
-}
-
-void imageRotate90(Image img, Image& rtImg) {
-    for (int i = 0; i < img.width; i++) {
-        for (int j = 0; j < img.height; j++) {
-            for (int k = 0; k < 3; k++) {
-                rtImg( img.height - 1 - j, i, k) = img(i, j, k);
-            }
-        }
-    }
-}
-void imageRotate180(Image img, Image& rtImg) {
-    for (int i = 0; i < img.width; i++) {
-        for (int j = 0; j < img.height; j++) {
-            for (int k = 0; k < 3; k++) {
-                rtImg( img.width - 1 - i, img.height - 1 - j, k) = img(i, j, k);
-            }
-        }
-    }
-}
-void imageRotate270(Image img, Image& rtImg) {
-    for (int i = 0; i < img.width; i++) {
-        for (int j = 0; j < img.height; j++) {
-            for (int k = 0; k < 3; k++) {
-                rtImg( j, img.width - 1 - i, k) = img(i, j, k);
-            }
-        }
-    }
-}
+// Functions to Flip an image
 
 void menu() {
     cout << "Welcome to our mini photo editor.\n"
             "(to start put your images in the cMake folder)\n" << endl;
 
     string imgName, ex;
-    cout << "Enter the name of your image:" << endl << ">>";
+    cout << "Enter the name of your image: (e.g. image.png)" << endl << ">>";
     getline(cin, imgName);
 
     size_t dotPos = imgName.find('.');
@@ -202,7 +101,7 @@ void menu() {
             grayImg.saveImage(imgName);
         }
         else {
-            string newName, newEx;
+            string newName;
             cout << "Enter the name of new image:" << endl << ">>";
             getline(cin, newName);
 
@@ -224,6 +123,78 @@ void menu() {
     }
 
     else if (choice_1 == "2") { // Merge Two Images
+        string imgName_2;
+        cout << "Enter the name of your second image:" << endl << ">>";
+        getline(cin, imgName_2);
+
+        dotPos = imgName_2.find('.');
+        ex = imgName_2.substr(dotPos + 1);
+
+        //Checking if file extension is valid
+        while (dotPos == string::npos or !exCheck(ex)) {
+            cout << "Error: unsupported file extension, enter file name again:"
+                 << endl << ">>";
+
+            getline(cin, imgName_2);
+            dotPos = imgName_2.find('.');
+            ex = imgName_2.substr(dotPos + 1);
+        }
+
+        Image img_1(imgName), img_2(imgName_2);
+
+        if (img_1.width != img_2.width or img_1.height != img_2.height) {
+            // should do resizing here
+        }
+
+        Image tempImg1, tempImg2;
+        string filename1, filename2;
+        exChange(img_1, 1, ".jpg", filename1, tempImg1);
+        exChange(img_2, 2, ".jpg", filename2, tempImg2);
+
+        Image merged(img_1.width, img_1.height);
+        Merge(tempImg1, tempImg2, 0.5, merged);
+
+        remove(filename1.c_str());
+        remove(filename2.c_str());
+
+        string choice_2;
+        cout << "Save changes in a new images or first image:\n"
+                "1- Adjust first image\n"
+                "2- Save to a new image\n"
+                ">>";
+
+        getline(cin, choice_2);
+        while (choice_2 != "1" and choice_2 != "2") {
+            cout << "Please select a valid option:\n" << ">>";
+            getline(cin, choice_2);
+        }
+
+        if (choice_2 == "1") {
+            merged.saveImage(imgName);
+        }
+        else {
+            string newName;
+            cout << "Enter the name of new image:" << endl << ">>";
+            getline(cin, newName);
+
+            dotPos = newName.find('.');
+            ex = newName.substr(dotPos + 1);
+
+            //Checking if file extension is valid
+            while (dotPos == string::npos or !exCheck(ex)) {
+                cout << "Error: unsupported file extension, enter file name again:"
+                     << endl << ">>";
+
+                getline(cin, newName);
+                dotPos = newName.find('.');
+                ex = newName.substr(dotPos + 1);
+            }
+
+            merged.saveImage(newName);
+        }
+    }
+
+    else if (choice_1 == "3") {
 
     }
 }
@@ -258,5 +229,133 @@ void notes() {
         remove(filename2.c_str());
 
         mergedImg.saveImage("saved img/merged.jpg");
+    }
+}
+
+bool choiceCheck(const string& choice) {
+    vector<string> choices = {"0", "1", "2", "3", "4", "5"};
+
+    for (const string& i : choices) {
+        if (choice == i) {
+            return true;
+        }
+    }
+    return false;
+}
+bool exCheck(const string& ex) {
+    if (ex == "png") {
+        return true;
+    } else if (ex == "jpg") {
+        return true;
+    } else if (ex == "jpeg") {
+        return true;
+    } else if (ex == "bmp") {
+        return true;
+    }
+    return false;
+}
+void exChange(Image img, int counter, const string& ex, string& filename, Image& jpgImg) {
+    filename = "temp/tempPic";
+    filename += to_string(counter);
+    filename += ex;
+    img.saveImage(filename);
+
+    jpgImg.loadNewImage(filename);
+}
+void GrayScale(Image img, Image& grayImg) {
+    for (int i = 0; i < img.width; i++) {
+        for (int j = 0; j < img.height; j++) {
+
+            unsigned int grayAvg = 0;
+            for (int k = 0; k < 3; k++) {
+                grayAvg += img(i, j, k);
+            }
+
+            grayAvg /= 3;
+            for (int k = 0; k < 3; k++) {
+                grayImg(i, j, k) = grayAvg;
+            }
+        }
+    }
+}
+void Merge(Image img1, Image img2, float opacity, Image& mergedImg) {
+    for (int i = 0; i < img1.width; i++) {
+        for (int j = 0; j < img1.height; j++) {
+
+            double color;
+            for (int k = 0; k < 3; k++) {
+                color = (1 - opacity) * img1(i, j, k) + opacity * img2(i, j, k);
+                mergedImg(i, j, k) = color;
+            }
+        }
+    }
+}
+void Brightness(Image img, float bright, Image& brightImg) {
+    for (int i = 0; i < img.width; i++) {
+        for (int j = 0; j < img.height; j++) {
+            for (int k = 0; k < 3; k++) {
+                int bb = img(i, j, k) * bright;
+                if (bb > 255) {
+                    bb = 255;
+                }
+                brightImg(i, j, k) = bb;
+            }
+        }
+    }
+}
+void EdgeDetect(Image grayedImg, Image& edgeImg) {
+    for (int i = 0; i < grayedImg.width; i++) {
+        for (int j = 0; j < grayedImg.height; j++) {
+            if (grayedImg(i + 1, j, 0) - grayedImg(i, j, 0) > 10
+                or grayedImg(i, j + 1, 0) - grayedImg(i, j, 0) > 10) {
+                edgeImg(i, j, 0) = 0;
+                edgeImg(i, j, 1) = 0;
+                edgeImg(i, j, 2) = 0;
+            }
+            else {
+                edgeImg(i, j, 0) = 255;
+                edgeImg(i, j, 1) = 255;
+                edgeImg(i, j, 2) = 255;
+            }
+        }
+    }
+}
+void InvertColor(Image myimage, Image& img) {
+    for (int i=0;i<myimage.width;i++)
+    {
+        for (int j=0;j<myimage.height;j++)
+        {
+            for (int k=0;k<myimage.channels;k++)
+            {
+                img(i,j,k)=255-myimage(i,j,k);
+            }
+        }
+    }
+}
+void imageRotate90(Image img, Image& rtImg) {
+    for (int i = 0; i < img.width; i++) {
+        for (int j = 0; j < img.height; j++) {
+            for (int k = 0; k < 3; k++) {
+                rtImg( img.height - 1 - j, i, k) = img(i, j, k);
+            }
+        }
+    }
+}
+void imageRotate180(Image img, Image& rtImg) {
+    for (int i = 0; i < img.width; i++) {
+        for (int j = 0; j < img.height; j++) {
+            for (int k = 0; k < 3; k++) {
+                rtImg( img.width - 1 - i, img.height - 1 - j, k) = img(i, j, k);
+            }
+        }
+    }
+}
+void imageRotate270(Image img, Image& rtImg) {
+    for (int i = 0; i < img.width; i++) {
+        for (int j = 0; j < img.height; j++) {
+            for (int k = 0; k < 3; k++) {
+                rtImg( j, img.width - 1 - i, k) = img(i, j, k);
+            }
+        }
     }
 }
